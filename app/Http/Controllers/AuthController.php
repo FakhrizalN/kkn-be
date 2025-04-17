ae035151cf5ebcd586ac34d4e3f332751a887d5f
FakhrizalN <11221057@student.itk.ac.id>
2025-04-17 20:59:18 +0800

    membuat fitur login dan logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Users;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
@@ -17,8 +19,52 @@ class AuthController extends Controller
         $users = Users::create($fields);
 
         $data = [
-            'message' => 'User berhasil register.'
+            'message' => 'Users berhasil register.'
         ];
         return response()->json($data, 201);
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $Users = Users::where('email', $request->email)->first();
+
+        if (!$Users || !Hash::check($request->password, $Users->password)) {
+            return response()->json([
+                "message" => "Username atau password salah!"
+            ], 404);
+        }
+
+        $token = $Users->createToken($Users->email)->plainTextToken;
+
+        $data = [
+            "message" => "Login Berhasil"
+        ];
+
+        return response()->json($data, 200)->cookie(
+            'TOKENID',
+            $token,
+            1440,
+            null,
+            false,
+            false
+        );
+    }
+
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();
+
+        $data = [
+            "message" => "Logout berhasil"
+        ];
+
+        return response()->json($data, 200)->cookie(
+            "TOKENID",
+            null,
+            -1
+        );
     }
 }
