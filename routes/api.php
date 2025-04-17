ab8aef67d62bcb5b9f8779d5c79a723f0600eb83
FakhrizalN <11221057@student.itk.ac.id>
2025-04-17 20:41:31 +0800

    membuat register dan memperbaiki banyak hal

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    // Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    // Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
+    // Route::post('/otp', [AuthController::class, 'sendOtp'])->name('auth.sendOtp');
+    // Route::post('/otp/verify', [AuthController::class, 'verifyOtp'])->name('auth.verifyOtp');
+    // Route::get('/otp/verify-page', [AuthController::class, 'verifyOtpPage'])->name('auth.verifyOtpPage');
+});
