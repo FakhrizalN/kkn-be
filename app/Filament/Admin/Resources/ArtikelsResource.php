2ca4e0b7051c21888be041fa255b5a3675e0b599
NikWasHere <137050447+NikWasHere@users.noreply.github.com>
2025-05-10 15:39:17 +0800

    all resource done
    semua jenis sudh

---
+++ app/Filament/Admin/Resources/ArtikelsResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\ArtikelsResource\Pages;
+use App\Filament\Admin\Resources\ArtikelsResource\RelationManagers;
+use App\Models\Artikels;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArtikelsResource extends Resource
+{
+    protected static ?string $model = Artikels::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArtikels::route('/'),
+            'create' => Pages\CreateArtikels::route('/create'),
+            'edit' => Pages\EditArtikels::route('/{record}/edit'),
+        ];
+    }
+}
