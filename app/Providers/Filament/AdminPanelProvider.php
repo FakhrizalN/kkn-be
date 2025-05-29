8bc5b8aa82fd612027b69d76c6768af40ce83661
FakhrizalN <11221057@student.itk.ac.id>
2025-05-29 15:53:28 +0800

    change favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Emerald,
             ])
             ->brandName('Sungai Wain')
+            ->favicon('storage/favicon.png')
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
             ->pages([
